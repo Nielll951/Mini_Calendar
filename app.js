951b19c261781202a1750a7d4bdb1c2e8cfc702f
Nielll951 <rodina9180gaocheng@ukr.net>
2023-11-29 18:47:13 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// find elements
+const date = document.getElementById('date')
+const day = document.getElementById('day')
+const month = document.getElementById('month')
+const year = document.getElementById('year')
+
+// get today's date
+const today = new Date()
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const allMonths = ["January", "February", "March", "April", "May", "Jane", "July", "August", "September", "October", "November", "December"]
+
+// display current date on the page 
+date.innerHTML = (today.getDate()<10 ? "0" : "") + today.getDate()
+day.innerHTML = weekDays[today.getDay()]
+month.innerHTML = allMonths[today.getMonth()]
+year.innerHTML = today.getFullYear()
